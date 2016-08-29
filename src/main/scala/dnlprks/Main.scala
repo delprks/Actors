4b5c4cefb6abcc42392f1abd835952a9b117b490
Daniel Parks <daniel.parks@bbc.co.uk>
2016-08-29 22:27:30 +0100

    using futures for publishing

--- src/main/scala/dnlprks/Main.scala
+++ src/main/scala/dnlprks/Main.scala
@@ -1,13 +1,13 @@
 package dnlprks
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{ActorSystem, Props}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.postfixOps
 
 object Main extends App {
-  val system = ActorSystem("MySystem")
-  val actor = system.actorOf(Props[MyActor], name = "myactor")
-  actor ! "begin"
-  system.scheduler.schedule(5 seconds, 10 seconds, actor, "hi")
+  val system = ActorSystem("PublisherSystem")
+  val actor = system.actorOf(Props[PublisherActor], name = "publisher-actor")
+  actor ! "Start"
+  system.scheduler.schedule(5 seconds, 10 seconds, actor, "PublishToCloudWatch")
 }
---
+++ src/main/scala/dnlprks/PublisherActor.scala
@@ -0,0 +1,21 @@
+package dnlprks
+
+import java.util.Calendar
+import akka.actor.Actor
+import scala.util.{Failure, Success}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class PublisherActor extends Actor {
+  def receive: Receive = {
+    case "Start" => println("Starting the application at " + Calendar.getInstance().getTime.toString)
+    case "PublishToCloudWatch" => publishToCloudWatch onComplete {
+      case Success(s) => println(s)
+      case Failure(f) => println("an error occurred " + f.getMessage)
+    }
+  }
+
+  def publishToCloudWatch: Future[String] = Future {
+    "Publishing to CloudWatch at " + Calendar.getInstance().getTime.toString
+  }
+}
